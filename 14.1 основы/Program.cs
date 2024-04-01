7d0bd57136be98fcae91d66fd046b5fc355bac10
Bikmacs <macsim.shmelev@yandex.ru>
2024-04-01 10:45:05 +0500

    s

---
+++ 14.1 основы/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Введите размерность 'Стека'");
+            int capacity = int.Parse(Console.ReadLine()); // Исправление: правильное написание метода Parse
+
+            // Вызываем метод Stack, передавая в него введенное значение capacity
+            Stack(capacity);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка ввода.");
+        }
+    }
+
+    // Метод Stack, который принимает в качестве параметра размерность стека
+    static void Stack(int capacity)
+    {
+        Console.WriteLine("");
+        Stack<int> stack = new Stack<int>();
+
+        // Заполнение стека значениями от 1 до capacity, а не от 1 до неопределенной переменной n
+        for (int i = 1; i <= capacity; i++)
+        {
+            stack.Push(i);
+        }
+
+        // Извлечение из стека и вывод на экран
+        Console.WriteLine($"Размерность стека: {capacity}");
+        while (stack.Count > 0)
+        {
+            int poppedValue = stack.Pop();
+            Console.WriteLine(poppedValue);
+        }
+    }
+}
